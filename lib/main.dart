82404a75aa23aee832990821b85f9d2ff66382c2
abhi-y918 <abhinavy918@gmail.com>
2024-10-14 23:44:54 +0530

    work for the day.

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  ); //scaffold widget can be used to make it good
+}
+//ctrl+alt+L formate the code
