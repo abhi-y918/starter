fcf5e31f98e813bbc364fd861f9e75c44178982d
abhi-y918 <abhinavy918@gmail.com>
2024-10-23 00:11:50 +0530

    22/10/24 work

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+//
+import 'package:untitled2/gradient_container.dart';
 
 void main() {
+
   runApp(
-    const MaterialApp(
+     const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+        // backgroundColor: Colors.amberAccent,
+        body: GradientContainer(Color.fromARGB(244, 148, 130, 195),
+          Color.fromARGB(244, 54, 34, 108)),
+
     ),
-  ); //scaffold widget can be used to make it good
+  ),
+
+  );//scaffold widget can be used to make it good
 }
-//ctrl+alt+L formate the code
+
